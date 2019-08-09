ae9e4b82595b0f9831bf4342d6133621dea36541
mromiario <mromiario@gmail.com>
2019-08-09 09:49:04 +0700

    event&attractions

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,4 @@
+const Restaurant = require('./eventController')
 module.exports = {
-    
+    Restaurant
 }
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const routes = express.Router()
+const event= require('./event')
+
+routes.use('/ticketmaster', event)
 
 
 module.exports = routes
